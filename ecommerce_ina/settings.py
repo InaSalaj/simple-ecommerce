199dc0d363d779215b338beb0127a8b2bfe08599
ina <inanisalaj@gmail.com>
2023-02-20 11:31:24 +0100

    changes

--- ecommerce_ina/settings.py
+++ ecommerce_ina/settings.py
@@ -20,7 +20,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-(_yfz7k#qp8-0zex58i130&1heoq3)9bu_ybne+gy@q)l9un82"
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
